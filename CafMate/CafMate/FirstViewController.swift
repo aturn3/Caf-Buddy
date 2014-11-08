1aabda1bde3a800a0371aa00be70ba96a32537f7
Jacob Forster <jforster959@gmail.com>
2014-11-08 12:56:38 -0600

    inital commit from local git

---
+++ CafMate/CafMate/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CafMate
+//
+//  Created by Jacob Forster on 11/8/14.
+//  Copyright (c) 2014 St. Olaf ACM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
