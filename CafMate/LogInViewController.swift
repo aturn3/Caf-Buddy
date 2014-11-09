27a76a04676ee90b0adb9ea7dfbca664cef4f5ec
Jacob Forster <jforster959@gmail.com>
2014-11-08 22:08:14 -0600

    got a basic version of the create new meal page working - need to get Armaan and Joe's changes to have the user id and make it work tho

--- CafMate/LogInViewController.swift
+++ CafMate/LogInViewController.swift
@@ -13,7 +13,6 @@ class LogInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        createLayout()
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,28 +20,6 @@ class LogInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func createLayout() {
-        var gameScore = PFObject(className: "GameScore")
-        gameScore.setObject(1337, forKey: "score")
-        gameScore.setObject("Sean Plott", forKey: "playerName")
-        gameScore.saveInBackgroundWithBlock {
-            (success: Bool!, error: NSError!) -> Void in
-            if (success != nil) {
-                NSLog("Object created with id: \(gameScore.objectId)")
-            } else {
-                NSLog("%@", error)
-            }
-        }
-        
-        
-        /*var screenwidth = Float(self.view.frame.size.width)
-        var thewid = 200
-        var final = screenwidth - Float(half)
-        //var myTextField: UITextField = UITextField(frame: CGRect(x: screenwidth-thewid/2, y: 0, width: 200, height: 40.00))
-        var myTextField = UITextField(frame: CGRectMake(CGFloat(final), 0, 200, 40.0))
-        self.view.addSubview(myTextField)
-        myTextField.backgroundColor = UIColor.redColor()
-        myTextField.text = "some string"*/
-    }
+    
     
 }
